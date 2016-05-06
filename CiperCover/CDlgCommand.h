169861611aebd6bdebe40764dd97880f2292aec7
NaksFM <babij_av@fizmat.tnpu.edu.ua>
2016-05-06 10:17:35 +0300

    Update CDlgCommand.h

--- CiperCover/CDlgCommand.h
+++ CiperCover/CDlgCommand.h
@@ -1,11 +1,8 @@
 #pragma once
 #include <string>
 #include "CWorkspace.h"
-//Декларація типу функції, що буде використовуватися для виконання користувацьких команд
-//
 // typedef void (*myfunc)()
-// myfunc f те саме що (*f)()
-// typedef спрощує використання вказівників на ф-ції
+// myfunc f те ж що (*f)()
 typedef void (*ptDlgCommandFunc)(CWorkspace& ws);
 
 class CDlgCommand
@@ -13,13 +10,12 @@ class CDlgCommand
 public:
  CDlgCommand(const char* sName, ptDlgCommandFunc pFunc = NULL);
  ~CDlgCommand(void);
- //Отримання назви команди
+ //Отримуємо назву команди
  const char* GetName() { return m_sName.c_str(); }
  //Виклик зв"язаної функції
  void Run(CWorkspace& ws);
 private:
  //Назва команди
  std::string m_sName;
- //Вказівник на функцію, що буде використовуватися для виконання користувацьких команд
  ptDlgCommandFunc m_pFunc;
 };
