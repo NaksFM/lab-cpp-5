238bde0fb65d102d9820421c823fa6d253f02b89
NaksFM <babij_av@fizmat.tnpu.edu.ua>
2016-05-06 10:12:33 +0300

    Update CDataChain.h

--- CiperCover/CDataChain.h
+++ CiperCover/CDataChain.h
@@ -9,12 +9,11 @@ class CDataChain
 public:
 	CDataChain(void);
 	virtual ~CDataChain(void);
-	//віртуальна функція, що має генерувати повну послідовність цифр.
+	//віртуальна функція
 	virtual void Generate(int nDepth, int nMaxLength = 0) = 0;
 	//повертає довжину згенерованої послідовності цифр.
 	size_t GetLength();
 	//повертає позицію входження підстрічки sSubStr починаючи із nPos.
-	// -1, якщо підстрічки не знайдено
 	int Find(const char* sSubStr, int nPos = 0);
 	//повертає підстрічку починаючи із nPos довжиною nLength. або до кінця послідовності
 	std::string GetSubStr(int nPos, int nLength = -1);
@@ -22,7 +21,6 @@ public:
 	const char* GetFullString();
 	std::string setString(std::string& str);
 protected:
-	//стрічка із згенерованою послідовністю цифр
 	std::string m_sChain;
 };
 
