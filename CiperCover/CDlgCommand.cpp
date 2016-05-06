d1ce63ee3ebb9828a09b9ed3db24a3e114bf6839
NaksFM <babij_av@fizmat.tnpu.edu.ua>
2016-05-06 10:16:16 +0300

    Update CDlgCommand.cpp

--- CiperCover/CDlgCommand.cpp
+++ CiperCover/CDlgCommand.cpp
@@ -4,13 +4,12 @@
 #include "CDlgCommand.h"
 
 CDlgCommand::CDlgCommand(const char* sName, ptDlgCommandFunc pFunc):m_sName(sName), m_pFunc(pFunc){};
-// Може треба буде видалити вказівник на ф-цію
 CDlgCommand::~CDlgCommand(){
 	// delete m_pFunc;
 };
 
-// Потрібно реалізувати збереження стану
+// Реалізуємо збереження стану
 void CDlgCommand::Run(CWorkspace& ws){
-	// викликаємо зарезервовану ф-цію
+	// виклик зарезервованої ф-ції
 	(*m_pFunc)(ws);
 }
