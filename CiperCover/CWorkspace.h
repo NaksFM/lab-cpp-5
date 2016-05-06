98d395826d78afd049bdd150b57cb2657528b4a5
NaksFM <babij_av@fizmat.tnpu.edu.ua>
2016-05-06 10:23:16 +0300

    Update CWorkspace.h

--- CiperCover/CWorkspace.h
+++ CiperCover/CWorkspace.h
@@ -10,25 +10,20 @@ class CWorkspace
 public:
 	CWorkspace(CDataChain& data);
 	~CWorkspace(void);
-	//Ініціалізує повну послідовність цифр
+	//Ініціалізує послідовність цифр
 	void Init(int nDepth, int nLength);
-	//Повертає стрічку із повною послідовністю цифр.
+	//Повертає стрічку з послідовністю цифр.
 	const char* GetChainString();
-	//Зберігає стан Workspace у файл sFilepath
 	bool Save(std::string& sFilepath);
-	//Відновлює стан Workspace із файлу sFilepath
 	bool Load(std::string& sFilepath);
 
-	// МЕТОДИ ДЛЯ РОБОТИ З ЛІНКАМИ
 	 
 	//Добавляє лінк pLink у список у вибрану позицію nStartPos із довжиною nLength
 	bool AddLink(int nStartPos, int nLength, CLink* pLink);
 	//Видаляє лінк із списку у вибраній позиції(в списку) nPosInList
 	bool RemoveLink(int nPosInList);
-	//Повертає всі лінки, що присутні в списку
+	//Повертає всі лінки, що є в списку
 	bool GetAllLinks(LinksArray& aLinks);
-	// ?
-	//void showAllLinks();
 	CDataChain& getRefChain();
 private:
 	//Зсилка на контейнер (породжений від CDataChain) із послідовністю
